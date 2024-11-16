d3a94a660acafdef6e6b40a4c8384fbaa33a03f6
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-11-16 13:03:39 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '100MB',
+    },
+  },
+  images: {
+    domains: ['img.freepik.com', 'cloud.appwrite.io'],
+  },
 };
 
 export default nextConfig;
